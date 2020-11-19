e72ab33e86987ae69e6c7ce9ea5587cd12aa21a1
valjukov aleksandr <valyukov.ad@phystech.edu>
2020-11-20 00:05:36 +0300

    save state_dict

--- noise_classification/train.py
+++ noise_classification/train.py
@@ -39,7 +39,7 @@ def main():
     model_dir = os.path.dirname(args.model_save_path)
     if not os.path.exists(model_dir) and model_dir:
         os.mkdir(model_dir)
-    torch.save(model, args.model_save_path, _use_new_zipfile_serialization=False)
+    torch.save(model.state_dict(), args.model_save_path, _use_new_zipfile_serialization=False)
 
 
 if __name__ == "__main__":
