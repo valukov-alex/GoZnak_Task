0b6b089d8590f796eaa6bfb359e3afe65ad74c3c
valjukov aleksandr <valyukov.ad@phystech.edu>
2020-11-19 23:18:37 +0300

    ...

--- noise_classification/model.py
+++ noise_classification/model.py
@@ -1,70 +1,65 @@
 from torch import nn
 
 
-class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size):
-        super(CNNBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
-        self.dp = nn.Dropout(0.5)
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), 
+                               stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), 
+                               stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU()
+
+        self.downsample = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1),
+                                    stride=2)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = self.dp(x)
+        identity = x
 
-        return x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        identity = self.downsample(identity)
+        out += identity
+
+        out = self.relu2(out)
+
+        return out
 
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
+class SmallResNet(nn.Module):
+    def __init__(self, num_classes):
+        super(VerySmallResNet, self).__init__()
+        self.conv = nn.Conv2d(1, 64, (7, 7), stride=2, padding=3)
+        self.bn = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d((2, 2))
 
-        self.block1 = CNNBlock(1, 16, (3, 3))
-        self.pool1 = nn.MaxPool2d((2, 2))
-
-        self.block2 = CNNBlock(16, 32, (3, 3))
-        self.pool2 = nn.MaxPool2d((2, 2))
-
-        self.block3 = CNNBlock(32, 64, (3, 3))
-        self.pool3 = nn.MaxPool2d((2, 2))
-
-        self.block4 = CNNBlock(64, 128, (3, 3))
-        self.pool4 = nn.MaxPool2d((2, 2))
+        self.block = ResNetBlock(64, 128)
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(128, 256)
-        self.bn = nn.BatchNorm1d(256)
-        self.relu = nn.ReLU()
-        self.dp = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(256, 2)
+        self.fc = nn.Linear(128, num_classes)
 
     def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.pool(out)
 
-        x = self.block1(x)
-        x = self.pool1(x)
+        out = self.block(out)
 
-        x = self.block2(x)
-        x = self.pool2(x)
+        out = self.avg_pool(out)
+        out = self.flatten(out)
 
-        x = self.block3(x)
-        x = self.pool3(x)
-
-        x = self.block4(x)
-        x = self.pool4(x)
-
-        x = self.avg_pool(x)
-
-        x = self.flatten(x)
-
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.dp(x)
-        x = self.fc2(x)
-
-        return x
+        out = self.fc(out)
+        return out
